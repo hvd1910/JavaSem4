c9a26df98904d8c81b20dedaf8c3783dcbe34bda
Hồ Việt Đức <tenducql@gmail.com>
2024-06-05 13:16:02 +0700

    commit

--- BaiThiSpringBoot/src/main/java/respository/VehicleRespository.java
+++
@@ -1,7 +0,0 @@
-package respository;
-
-import model.Vehicle;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VehicleRespository extends JpaRepository<Vehicle, Integer> {
-}
